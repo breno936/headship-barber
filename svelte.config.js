869cab6fb0ece4424f3f2a73b4ebfdf838f802d9
flb1ca <flb1ca@bosch.com>
2024-06-24 10:13:15 -0300

    upload route font

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias:{
-			$components:'src/lib/components'
+			$components:'src/lib/components',
+			$static:'static/fonts'
 		}
 	}
 };
